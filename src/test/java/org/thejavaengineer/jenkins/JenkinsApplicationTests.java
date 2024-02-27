bd00b280027a83a61f5a153919dc88cf109288b4
Vipul Kumawat <vipul.kumawat7@gmail.com>
2024-02-27 19:12:14 +0530

    initial commit

---
+++ src/test/java/org/thejavaengineer/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thejavaengineer.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
