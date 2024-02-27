68e59cab193e44e4c3081633a3540ef15e0c6b63
Vipul Kumawat <vipul.kumawat7@gmail.com>
2024-02-27 19:41:57 +0530

    added controller and test case.

---
+++ src/main/java/org/thejavaengineer/jenkins/HelloWorldController.java
@@ -0,0 +1,13 @@
+package org.thejavaengineer.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
---
+++ src/test/java/org/thejavaengineer/jenkins/HelloWorldControllerTest.java
@@ -0,0 +1,24 @@
+package org.thejavaengineer.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHelloWorld() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello, World!"));
+    }
+}
